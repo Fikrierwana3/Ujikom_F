09eb10cb1892c4a41ad27f534b445e22714c4038
Fikrierwana3 <fikrierwana3@gmail.com>
2025-04-21 16:12:04 +0700

    update

--- resources/views/include/frontend/tranding.blade.php
+++ resources/views/include/frontend/tranding.blade.php
@@ -11,33 +11,35 @@
             </div>
 
             <div class="row">
-                <!-- Trending Top -->
+                <!-- Trending Top - Bagian Kiri -->
                 <div class="col-lg-8">
                     <div class="trending-top mb-30">
                         @foreach ($tranding as $data)
                         <div class="trend-top-img">
-                            <img src="{{ asset('/img/berita/' . $data->cover) }}" alt="{{ $data->judul }}">
-                            <div class="trend-top-cap">
-                                <span>{{ $data->kategori->nama }}</span>
-                                <h2><a href="{{ route('berita.detail', $data->id) }}">{{ $data->judul }}</a></h2>
-                                <p>{{ Str::limit($data->isi, 70) }}</p>
-                            </div>
+                            <a href="{{ route('berita.detail', $data->id) }}" class="clickable-block">
+                                <img src="{{ asset('/img/berita/' . $data->cover) }}" alt="{{ $data->judul }}">
+                                <div class="trend-top-cap">
+                                    <span>{{ $data->kategori->nama }}</span>
+                                    <h2>{{ $data->judul }}</h2>
+                                    <p>{{ Str::limit($data->isi, 70) }}</p>
+                                </div>
+                            </a>
                         </div>
                         @endforeach
                     </div>
                 </div>
 
-                <!-- Recent News -->
+                <!-- Recent News - Bagian Kanan -->
                 <div class="col-lg-4">
                     @foreach ($recent as $data)
                     <div class="trand-right-single d-flex">
-                        <div class="trand-right-img">
+                        <a href="{{ route('berita.detail', $data->id) }}" class="trand-right-img">
                             <img src="{{ asset('/img/berita/' . $data->cover) }}" alt="{{ $data->judul }}" width="160">
-                        </div>
-                        <div class="trand-right-cap">
+                        </a>
+                        <a href="{{ route('berita.detail', $data->id) }}" class="trand-right-cap">
                             <span class="color1">{{ $data->kategori->nama }}</span>
-                            <h4><a href="{{ route('berita.detail', $data->id) }}">{{ Str::limit($data->judul, 30) }}</a></h4>
-                        </div>
+                            <h4>{{ Str::limit($data->judul, 30) }}</h4>
+                        </a>
                     </div>
                     @endforeach
                 </div>
@@ -46,3 +48,29 @@
     </div>
 </div>
 <!-- Trending Area End -->
+
+<style>
+    /* CSS untuk memastikan link bisa diklik dengan benar */
+    .clickable-block {
+        display: block;
+        position: relative;
+        text-decoration: none;
+        color: inherit;
+    }
+
+    .trend-top-img a,
+    .trand-right-single a {
+        z-index: 1;
+    }
+
+    .trand-right-img {
+        display: block;
+        margin-right: 15px;
+    }
+
+    .trand-right-cap {
+        display: block;
+        text-decoration: none;
+        color: inherit;
+    }
+</style>
